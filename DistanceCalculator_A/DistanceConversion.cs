cd82fc963436dc75d35ffaffa85d9b7be0c27c5c
David Allison <Dallison@conestogac.on.ca>
2021-09-23 22:17:06 -0400

    Add project files.

---
+++ DistanceCalculator_A/DistanceConversion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceCalculator_A
+{
+    public static class DistanceConversion
+    {
+        /*
+        public static double Convert(double value, string convertFrom, string convertTo)
+        {
+
+        }
+        */
+
+        /*
+        private static string ModifyInput(string input) 
+        {
+
+        }
+        */
+
+        /*
+        private static double GetMultiplier(string convertFrom, string convertTo)
+        {
+
+        }
+        */
+    }
+}
---
+++ DistanceCalculator_A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistanceCalculator_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
