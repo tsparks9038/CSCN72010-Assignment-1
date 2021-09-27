084ebffe2c88fa6c6f365b10b68f8168d435eee1
David Allison <Dallison@conestogac.on.ca>
2021-09-27 11:53:08 -0400

    Fix namespace typo.

--- DistanceCalculator_B/DistanceConversion.cs
+++ DistanceCalculator_B/DistanceConversion.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistanceCalculator_A
+namespace DistanceCalculator_B
 {
     public static class DistanceConversion
     {
